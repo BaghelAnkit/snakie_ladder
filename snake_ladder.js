ada36e9399a73d3af7a0b8e19c20430f88f67976
anubhav singh <anubhavsingh230@gmail.com>
2023-03-12 01:37:55 +0530

    rolls the die and get a number

--- snake_ladder.js
+++ snake_ladder.js
@@ -1,2 +1,7 @@
  let start = 0
  console.log("pl_1 is at start", start)
+     //  let temp = Math.round(Math.random() * 6)
+ for (let t = 1; t < 100; t++) {
+     let a = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+     console.log("temp", a)
+ }
