71f8ee33dc1b6724a2b41554f12eb35c3536c998
anubhav singh <anubhavsingh230@gmail.com>
2023-03-12 19:50:01 +0530

    check noplay ,ladder,snake and takes staps

--- snake_ladder.js
+++ snake_ladder.js
@@ -1,7 +1,22 @@
- let start = 0
+ let start = 0,
+     poss
  console.log("pl_1 is at start", start)
-     //  let temp = Math.round(Math.random() * 6)
- for (let t = 1; t < 100; t++) {
-     let a = Math.floor(Math.random() * (6 - 1 + 1) + 1)
-     console.log("temp", a)
+ let a = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+ console.log("temp", a)
+ let noplay = 0,
+     ladder = 1,
+     snake = 2
+ let b = Math.round(Math.random() * 2)
+ console.log("temp2", b)
+ if (b == ladder) {
+     poss = start + a
+     console.log("poss", poss)
+ } else if (b == noplay) {
+     poss = start
+     console.log("poss", poss)
+
+ } else {
+     poss = start - a
+     console.log("poss", poss)
+
  }
