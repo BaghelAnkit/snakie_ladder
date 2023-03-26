59a8b487b785c9532f25d021e31b61ff6f35ad82
anubhav singh <anubhavsingh230@gmail.com>
2023-03-27 01:13:43 +0530

    playing with  two players

--- snake_ladder.js
+++ snake_ladder.js
@@ -1,33 +1,38 @@
- let poss = 0
- winning_poss = 100
- count = 0
-
- console.log("pl_1 is at start", poss)
+ let poss_ply1 = 0
+ let poss_ply2 = 0
+ let winning_poss = 100
  const noplay = 0,
      ladder = 1,
      snake = 2
- while (poss < winning_poss) {
-     count++
+ let active_player = true
+ while (poss_ply1 < winning_poss && poss_ply2 < winning_poss) {
      let dice = Math.floor(Math.random() * (6 - 1 + 1) + 1)
-     console.log("dice", dice)
-
      let option = Math.round(Math.random() * 2)
-     console.log("option", option)
-     if (option == ladder && poss + dice <= 100) {
-         poss = poss + dice
-         console.log("poss", poss)
-         console.log("adding")
+     if (option == ladder) {
+         if (active_player == true && poss_ply1 + dice <= winning_poss) {
+             poss_ply1 = poss_ply1 + dice
+         }
+         if (active_player == false && poss_ply2 + dice <= winning_poss) {
+             poss_ply2 = poss_ply2 + dice
+         }
+     }
+     if (option == snake) {
+         if (active_player == true && poss_ply1 + dice <= winning_poss) {
+             poss_ply1 = poss_ply1 - dice
+         }
+         if (active_player == false && poss_ply2 + dice <= winning_poss) {
+             poss_ply2 = poss_ply2 - dice
+         }
+         active_player = !active_player
      }
      if (option == noplay) {
-         //  poss = poss
-         console.log("noplay")
+         active_player = !active_player
      }
-     if (option == snake && dice - poss > 0) {
-         poss = poss - dice
-         console.log("poss", poss)
-     }
-
-
-
  }
- console.log("count", count)
+ console.log("ply1", poss_ply1)
+ console.log("ply2", poss_ply2)
+ if (poss_ply1 == winning_poss) {
+     console.log("plahyer 1 wins the game")
+ } else {
+     console.log("player 2 wins the game")
+ }
