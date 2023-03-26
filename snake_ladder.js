530b9abca645cf40a375c6289ed5939fa041b412
anubhav singh <anubhavsingh230@gmail.com>
2023-03-26 20:52:40 +0530

    reached winnig posstion

--- snake_ladder.js
+++ snake_ladder.js
@@ -1,22 +1,31 @@
- let start = 0,
-     poss
- console.log("pl_1 is at start", start)
- let a = Math.floor(Math.random() * (6 - 1 + 1) + 1)
- console.log("temp", a)
- let noplay = 0,
+ let poss = 0
+ winning_poss = 100
+
+ console.log("pl_1 is at start", poss)
+ const noplay = 0,
      ladder = 1,
      snake = 2
- let b = Math.round(Math.random() * 2)
- console.log("temp2", b)
- if (b == ladder) {
-     poss = start + a
-     console.log("poss", poss)
- } else if (b == noplay) {
-     poss = start
-     console.log("poss", poss)
 
- } else {
-     poss = start - a
-     console.log("poss", poss)
+
+
+ while (poss < winning_poss) {
+     let dice = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+     console.log("dice", dice)
+
+     let option = Math.round(Math.random() * 2)
+     console.log("option", option)
+     if (option == ladder && poss + dice <= 100) {
+         poss = poss + dice
+         console.log("poss", poss)
+         console.log("adding")
+     }
+     if (option == noplay) {
+         //  poss = poss
+         console.log("noplay")
+     }
+     if (option == snake && dice - poss > 0) {
+         poss = poss - dice
+         console.log("poss", poss)
+     }
 
  }
